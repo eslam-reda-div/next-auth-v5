48aaadec3a367d021195f0b1aa63bad8d6c329fc
eslam reda <eslam.reda.div@gmail.com>
2024-01-13 12:51:16 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           A simple authentication service
         </p>
         <div>
-          <LoginButton  asChild>
+          <LoginButton mode="modal" asChild>
             <Button variant="secondary" size="lg">
               Sign in
             </Button>
